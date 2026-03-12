50c70a7bdcd7ef17735cba02237d534e3a280a54
yanqne <giangneeee1308@gmail.com>
2026-03-12 16:56:26 +0700

    Basic
    Basic project need

---
+++ LearningLMS/src/main/java/com/GiangTruong/LearningLMS/LearningLmsApplication.java
@@ -0,0 +1,13 @@
+package com.GiangTruong.LearningLMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningLmsApplication.class, args);
+	}
+
+}
---
+++ LearningLMS/src/test/java/com/GiangTruong/LearningLMS/LearningLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GiangTruong.LearningLMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
